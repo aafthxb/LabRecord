89e859d20f4ea1c216fbeb133e1cf2f1104b8111
Aafthab Rahman <115390236+aafthxb@users.noreply.github.com>
2026-08-01 18:34:08 +0530

    Delete programs/Java/HelloWorld.java

--- programs/Java/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-// Hello World
-// Print "Hello, World!" to the console.
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-}

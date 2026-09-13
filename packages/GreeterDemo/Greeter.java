1eb2bc6baf0eea6c963f24f9ff4a0c6623e8e3b7
Aafthab Rahman <115390236+aafthxb@users.noreply.github.com>
2026-09-13 14:27:52 +0530

    Add files via upload

---
+++ packages/GreeterDemo/Greeter.java
@@ -0,0 +1,13 @@
+// Greeter
+// Small reusable class that builds a greeting message.
+public class Greeter {
+    private final String name;
+
+    public Greeter(String name) {
+        this.name = name;
+    }
+
+    public String greet() {
+        return "Hello, " + name + "!";
+    }
+}

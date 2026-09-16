358e5e17b84cb82243b21a0fa7dcf346534dc15a
Aafthab Rahman <115390236+aafthxb@users.noreply.github.com>
2026-09-16 17:25:29 +0530

    Delete packages/Java directory

--- packages/Java/GreeterDemo/Main.java
+++
@@ -1,8 +0,0 @@
-// Main
-// Entry point that wires up Greeter and prints its output.
-public class Main {
-    public static void main(String[] args) {
-        Greeter greeter = new Greeter("LabRecord");
-        System.out.println(greeter.greet());
-    }
-}
--- packages/Java/example/Hello.java
+++
@@ -1,9 +0,0 @@
-// Hello
-
-package myPackage;
-
-public class Hello {
-    public static void main(String[] args) {
-        System.out.println("Hello from myPackage!");
-    }
-}   

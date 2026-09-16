98068e601bea0c6f6b2f47c15ed101998cdef650
Aafthab Rahman <115390236+aafthxb@users.noreply.github.com>
2026-09-17 00:45:01 +0530

    Editor: add package Java/insystems/ (1 file)

---
+++ programs/Java/packages/insystems/Gumball.java
@@ -0,0 +1,15 @@
+// Gumball
+
+package insystems;
+
+public class Gumball {
+    private int count;
+
+    public Gumball(int count) {
+        this.count = count;
+    }
+
+    public int getCount() {
+        return count;
+    }
+}   

44a9b8d431e37b189baa2cf33385015603aae140
Aafthab Rahman <115390236+aafthxb@users.noreply.github.com>
2026-09-17 00:19:47 +0530

    Add Main.java via LabRecord editor

---
+++ programs/Java/Main.java
@@ -0,0 +1,10 @@
+// package use
+
+import insystems.gumball.Gumball; // Import specific class
+
+public class Main {
+    public static void main(String[] args) {
+        Gumball gb = new Gumball(5);
+        System.out.println("Gumballs: " + gb.getCount());
+    }
+}   
